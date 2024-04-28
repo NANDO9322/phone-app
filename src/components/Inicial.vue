451c1e15a4b3656b3ccf59532403147e23046e22
Paulo Souza <paulo@agencia56k.com.br>
2024-04-28 12:35:34 -0300

    new

--- src/components/Inicial.vue
+++ src/components/Inicial.vue
@@ -5,16 +5,13 @@
     </button>
     <button @click="recebendo">Recebendo ligacao</button>
 
-    <div
-      v-if="visible"
-      :class="{
-        'telefone-topper': true,
-        'calling-state':
-          stateCall === 'calling' ||
-          stateCall === 'inCall' ||
-          stateCall === 'receiving',
-      }"
-      style="
+    <div v-if="visible" :class="{
+      'telefone-topper': true,
+      'calling-state':
+        stateCall === 'calling' ||
+        stateCall === 'inCall' ||
+        stateCall === 'receiving',
+    }" style="
         height: 55vh;
         width: 30vh;
         border-radius: 30px;
@@ -22,35 +19,21 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
-      "
-    >
-      <div
-        v-if="stateCall === 'initial'"
-        style="
+      ">
+      <div v-if="stateCall === 'initial'" style="
           height: 100%;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        "
-      >
+        ">
         <div class="dial-input" style="">
           <div class="input-container, column" style="margin-bottom: 3vh">
-            <span style="color: white; margin-bottom: 10px"
-              >DISQUE POR AQUI</span
-            >
-            <input
-              class="box"
-              type="text"
-              v-model="numeroDiscado"
-              style="height: 3vh; width: 25vh"
-            />
+            <span style="color: white; margin-bottom: 10px">DISQUE POR AQUI</span>
+            <input class="box" type="text" v-model="numeroDiscado" style="height: 3vh; width: 25vh" />
           </div>
         </div>
-        <div
-          class="dial-pad"
-          style="background-color: white; width: 30vh; padding-top: 2vh"
-        >
+        <div class="dial-pad" style="background-color: white; width: 30vh; padding-top: 2vh">
           <div class="row">
             <button @click="addDigit(1)" class="circular-button">1</button>
             <button @click="addDigit(2)" class="circular-button">
@@ -83,154 +66,105 @@
             </button>
           </div>
           <div class="row">
-            <button
-              @click="addDigit('*')"
-              style="border: none"
-              class="circular-button"
-            >
+            <button @click="addDigit('*')" style="border: none" class="circular-button">
               *
             </button>
             <button @click="addDigit(0)" class="circular-button">0</button>
-            <button
-              @click="addDigit('#')"
-              style="border: none"
-              class="circular-button"
-            >
+            <button @click="addDigit('#')" style="border: none" class="circular-button">
               #
             </button>
           </div>
           <div class="actions, row">
-            <button
-              @click="ligar"
-              class="ligar-button"
-              style="margin-left: 6vh"
-            >
+            <button @click="ligar" class="ligar-button" style="margin-left: 6vh">
               <img src="../assets/icons/ligar.svg" alt="" />
             </button>
-            <button
-              @click="limpar"
-              style="background-color: white; border: none; margin-left: 2vh"
-            >
+            <button @click="limpar" style="background-color: white; border: none; margin-left: 2vh">
               <img src="../assets/icons/apagar.svg" alt="" />
             </button>
           </div>
         </div>
       </div>
 
-      <div
-        v-if="stateCall === 'calling'"
-        style="
+      <div v-if="stateCall === 'calling'" style="
           height: 100%;
           display: flex;
           flex-direction: column;
           align-items: center;
-        "
-      >
+        ">
         <!--<span>ligando</span>
         <button @click="desligar">cancelar</button>
         <button @click="atender">atendeu</button>-->
         <div class="dial-input" style="">
-          <div
-            class="input-container, column"
-            style="margin-bottom: 3vh; font-size: 2vh"
-          >
+          <div class="input-container, column" style="margin-bottom: 3vh; font-size: 2vh">
             <span style="color: white; margin-bottom: 10px">LIGANDO</span>
           </div>
           <div @click="recebendo">
-            <img
-              src="/src/assets/icons/predio.jpg"
-              alt=""
-              style="opacity: 1; border: none"
-            />
+            <div class="efeito">
+              <div class="efeito__container">
+                <div class="efeito__container__inside">
+                  <img src="../assets/icons/ligar.svg" alt="" />
+                </div>
+              </div>
+            </div>
           </div>
           <div style="margin-top: 3vh; font-size: 2.5vh">
             <span style="color: white">Marcos</span>
           </div>
-          <div
-            @click="desligar"
-            style="
+          <div  >
+            <button @click="desligar" style="
               display: flex;
               justify-content: center;
               text-align: center;
               align-items: center;
               background-color: red;
               border-radius: 3vh;
-            "
-          >
-            <button>
-              <img
-                src="../assets/icons/recusar.svg"
-                style="height: 4vh; width: 4vh"
-              />
+              width: 100%;
+              gap: 15px;
+              font-weight: bolder;
+            ">
+              <img src="../assets/icons/recusar.svg" style="height: 4vh; width: 4vh" />
+              <span style="color: white">CANCELAR</span>
             </button>
-            <span style="color: white">CANCELAR</span>
           </div>
         </div>
       </div>
 
       <div v-if="stateCall === 'receiving'">
         <div class="dial-input" style="">
-          <div
-            class="input-container, column"
-            style="margin-bottom: 3vh; font-size: 2vh"
-          >
-            <span style="color: white; margin-bottom: 10px"
-              >Chamada recebida</span
-            >
+          <div class="input-container, column" style="margin-bottom: 3vh; font-size: 2vh">
+            <span style="color: white; margin-bottom: 10px">Chamada recebida</span>
           </div>
-          <div>
-            <img
-              src="/src/assets/icons/predio.jpg"
-              alt=""
-              style="opacity: 1; border: none"
-            />
+          <div style="display: flex; align-items: center; justify-content: center;">
+            <div class="efeito">
+              <div class="efeito__container">
+                <div class="efeito__container__inside">
+                  <img src="../assets/icons/ligar.svg" alt="" />
+                </div>
+              </div>
+            </div>
           </div>
           <div style="margin-top: 3vh; font-size: 2.5vh">
             <span style="color: white">Marcos</span>
           </div>
-          <div
-            @click="atender"
-            style="
-              display: flex;
-              justify-content: center;
-              text-align: center;
-              align-items: center;
-              background-color: green;
-              border-radius: 3vh;
-            "
-          >
-            <button>
-              <img src="../assets/icons/atender.svg" alt="" />
+
+          <div style="display: flex; flex-direction: column; gap: 15px; align-items: center">
+            <button @click="atender"
+              style="width: 100%; border: none; font-size: 25px; padding: 10px; display: flex; justify-content: center; text-align: center; align-items: center; background-color: green; border-radius: 3vh; gap: 15px;">
+              <img src="../assets/icons/ligar.svg" alt="" />
+              <span style="font-weight: bolder; color: #FFFFFF;">ATENDER</span>
             </button>
-            <span>ATENDER</span>
-          </div>
-          <div
-            @click="desligar"
-            style="
-              display: flex;
-              justify-content: center;
-              text-align: center;
-              align-items: center;
-              background-color: red;
-              border-radius: 3vh;
-            "
-          >
-            <button>
-              <img
-                src="../assets/icons/recusar.svg"
-                style="height: 4vh; width: 4vh"
-              />
+
+
+            <button @click="desligar"
+              style="width: 75%; border: none; display: flex; justify-content: center; text-align: center; align-items: center; background-color: red; border-radius: 3vh; gap: 15px;">
+              <img src="../assets/icons/recusar.svg" style="height: 4vh; width: 4vh" />
+              <span style="font-weight: bolder; color: #FFFFFF;">REJEITAR</span>
             </button>
-            <span style="color: white">CANCELAR</span>
           </div>
         </div>
       </div>
 
-      <div
-        v-if="stateCall === 'inCall'"
-        style="color: white"
-        class="column, input-container"
-      >
+      <div v-if="stateCall === 'inCall'" style="color: white" class="column, input-container">
         <div class="input-container">
           <div>
             <span>Marcos</span>
@@ -243,7 +177,7 @@
             <span>teclado</span>
           </div>
           <button @click="desligar" class="my-button">
-            <span class="material-symbols-outlined"> call_end </span>
+            <img src="../assets/icons/recusar.svg" style="height: 4vh; width: 4vh" />
             <span>DESLIGAR</span>
           </button>
         </div>
@@ -429,7 +363,10 @@ export default {
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
-  display: inline-block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
   font-size: 16px;
   margin: 4px 2px;
   cursor: pointer;
@@ -442,5 +379,90 @@ export default {
 
 .material-symbols-outlined {
   font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 24;
+}
+
+
+
+
+.efeito__container__inside {
+  width: 100px;
+  height: 100px;
+  animation: efeito-inside 3s infinite linear;
+}
+
+.efeito__container {
+  width: 150px;
+  height: 150px;
+  animation: efeito-container 3s infinite linear;
+  animation-delay: -1s;
+  /* Atraso para iniciar após 1 segundo da animação anterior */
+}
+
+.efeito {
+  width: 200px;
+  height: 200px;
+  animation: efeito 3s infinite linear;
+  animation-delay: -2s;
+  /* Atraso para iniciar após 2 segundos da animação anterior */
+}
+
+.efeito,
+.efeito__container,
+.efeito__container__inside {
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@keyframes efeito-inside {
+  0% {
+    border-width: 2px;
+    box-shadow: 0 0 0 2px rgba(0, 200, 170, 0.5);
+  }
+
+  50% {
+    border-width: 2px;
+    box-shadow: 0 0 0 2px rgba(0, 200, 170, 0.5);
+  }
+
+  100% {
+    border-width: 2px;
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+}
+
+@keyframes efeito-container {
+  0% {
+    border-width: 2px;
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+
+  50% {
+    border-width: 2px;
+    box-shadow: 0 0 0 2px rgba(0, 200, 170, 0.5);
+  }
+
+  100% {
+    border-width: 2px;
+    box-shadow: 0 0 0 2px rgba(0, 200, 170, 0.5);
+  }
+}
+
+@keyframes efeito {
+  0% {
+    border-width: 2px;
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+
+  50% {
+    border-width: 2px;
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+
+  100% {
+    border-width: 2px;
+    box-shadow: 0 0 0 2px rgba(0, 200, 170, 0.5);
+  }
 }
 </style>
